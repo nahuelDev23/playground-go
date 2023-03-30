c2d285fa3cebddddc40493a884b9738bfd45a746
NahuelDev23 <nahuel.dev.23@gmail.com>
2023-03-30 17:31:02 -0300

    token recived

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,18 +4,28 @@ import (
 	"log"
 	"net/http"
 
+	//"github.com/nahueldev23/playground/auth"
 	"github.com/nahueldev23/playground/api"
 	"github.com/nahueldev23/playground/database"
 )
 
 func main() {
+
+	//	err := auth.LoadFiles("app.rsa", "app.rsa.pub")
+	//if err != nil {
+	//	log.Printf("error el cargar los RSA , %v \n", err)
+	//}
+
 	driver := database.Postgres
 	database.New(driver)
 	store, err := database.DAOPerson(driver)
+	if err != nil {
+		log.Printf("error en DAO Person, %v \n", err)
+	}
 	storeLogin, err := database.DAOLogin(driver)
 
 	if err != nil {
-		log.Printf("error en daoperson, %v \n", err)
+		log.Printf("error en DAO Login, %v \n", err)
 	}
 
 	mux := http.NewServeMux()
